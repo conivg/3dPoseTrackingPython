39a17c3e7f4c684705fcbe79c1fffb3e5f91a1d2
conivg <conivaldez438@gmail.com>
2023-01-16 13:17:40 -0800

    speed

--- distance.py
+++ distance.py
@@ -1,5 +1,6 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import time
 import math
 import numpy as np
 import cvzone
@@ -7,7 +8,12 @@ import cvzone
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
+initialTime = 0
+initialDistance = 0
+changeInTime = 0
+changeInDistance = 0
 
+listSpeed = []
 #Hand Detector
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
@@ -16,8 +22,7 @@ x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
 y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 coff = np.polyfit(x, y, 2)
 # y = Ax^2 + Bx + C
-
-
+initialTime = time.time()
 #loop
 while True:
     success, img = cap.read()
@@ -32,7 +37,13 @@ while True:
         distance = int(math.sqrt((y2-y1)**2 + (x2-x1)**2+ (z2-z1)**2))
         A, B , C = coff
         distanceCM = A*distance**2 + B*distance + C
+        distanceM = (A * distance ** 2 + B * distance + C)/100
+        actualTime = time.time() - initialTime
+        speed = distanceM/actualTime
+        print(speed)
+        initialTime = time.time()
         #print(distanceCM, distance)
-        cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x + 5,y - 10))
+        cvzone.putTextRect(img, f'{round(speed),2} m/s', (x + 5, y - 10))
+        #cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x + 5,y - 10))
     cv2.imshow("image",img)
     cv2.waitKey(1)
