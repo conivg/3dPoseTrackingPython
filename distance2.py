d208d835fcd238e313ae93ac0f9a1a6c86aeb3b6
conivg <conivaldez438@gmail.com>
2023-01-22 11:51:19 -0800

    cm/seg

--- distance2.py
+++ distance2.py
@@ -1,6 +1,7 @@
 import cv2
 import cvzone
 import math
+import time
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 cap = cv2.VideoCapture(0)
@@ -13,19 +14,21 @@ y1 = 0
 z1 = 0
 #Find Functions
 #raw distance
-x = [232,151,131,125,105,90,83,75,68]
+x = [300,245,200,170,145,130,112,103,93,87,80,75,70,67,62,59,57]
 #Value at centimiters
-y = [20,25,30,35,40,45,50,55,60]
+y = [20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]
 coff = np.polyfit(x,y,2)
 listPosX = []
 listPosY = []
 listPosZ = []
+listDespCM = []
 listVel = []
 listvx = []
 listvy = []
 listvz=[]
 listAcc=[]
 listJerk=[]
+listDespCM.append(0)
 listPosX.append(0)
 listPosY.append(0)
 listPosZ.append(0)
@@ -35,11 +38,13 @@ listvy.append(0)
 listvz.append(0)
 listAcc.append(0)
 listJerk.append(0)
+start_time = time.time()
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
 
     if hands:
+
         lmList = hands[0]['lmList']
         x, y, w, h = hands[0]['bbox']
         #x1,y1,z1 = lmList[5]
@@ -48,35 +53,44 @@ while True:
         listPosX.append(x2)
         listPosY.append(y2)
         listPosZ.append(z2)
+
         #displacement
         difX = listPosX[-1]-listPosX[-2]
         difY = listPosY[-1]-listPosY[-2]
         difZ = listPosZ[-1]-listPosZ[-2]
         dif = math.sqrt((difX)**2 + (difY)**2 + (difZ)**2)
-        ##Velocity
-        velX = (difX) / (1 / 30)
-        velY = (difY) / (1 / 30)
-        velZ = (difZ) / (1 / 30)
-        vel = math.sqrt((velX) ** 2 + (velY) ** 2 + (velZ) ** 2)
-        listvx.append(velX)
-        listvy.append(velY)
-        listvz.append(velZ)
+        A,B,C = coff
+        distanceCM = A*dif**2 + B*dif + C
+        listDespCM.append(distanceCM)
+        desp = listDespCM[-1] - listDespCM[-2]
+        actualtime = time.time()
+        timer = actualtime-start_time
+        print("time:", timer)
+        #Velocity
+        #velX = (difX) / (1/30)
+        #velY = (difY) / (1/30)
+        #velZ = (difZ) / (1/30)
+        vel = abs(desp/timer)
+        #listvx.append(velX)
+        #listvy.append(velY)
+        #listvz.append(velZ)
         listVel.append(vel)
         #Acceleration
-        acc = ((listVel[-1] - listVel[-2]) / (1 / 30))
+        acc = round(((listVel[-1] - listVel[-2]) / timer)/100, 3)
         listAcc.append(acc)
-        jerk = ((listAcc[-1] - listAcc[-2]) / (1 / 30))  # (px/frame^3)
+
+        jerk = round(((listAcc[-1] - listAcc[-2]) / timer)/10000, 2)  # (px/frame^3)
         #listJerk.append(jerk)
         ci = (math.sqrt(x2 ** 2 + y2 ** 2 + z2 ** 2)) / (math.sqrt(xP ** 2 + yP ** 2 + zP ** 2))
         weight = vel ** 2
-        print("speed(tf):", listVel[-1], "speed(ti):", listVel[-2],"acc:", acc, "jerk:", jerk, "weight: ", weight)
+        print("speed(tf):", round(listVel[-1], 3), "speed(ti):", round(listVel[-2],3),"acc:", acc, "jerk:", jerk, "weight: ", weight)
 
-        cvzone.putTextRect(img, "speed:" f'{round(vel,2)} p/f', (x + 400, y - 50))
-        cvzone.putTextRect(img, "acc:" f'{int(acc)} p/f^2', (x + 400, y))
-        cvzone.putTextRect(img, "jerk:" f'{round(jerk, 2)} p/f^3', (x + 400, y + 50))
+        cvzone.putTextRect(img, "speed:" f'{int(vel)} cm/s', (x + 400, y - 50))
+        cvzone.putTextRect(img, "acc:" f'{int(acc)} cm/s^2', (x + 400, y))
+        cvzone.putTextRect(img, "jerk:" f'{round(jerk, 2)} cm/s^3', (x + 400, y + 50))
         cvzone.putTextRect(img, "CI:" f'{round(ci, 2)}', (x + 400, y + 100))
-        cvzone.putTextRect(img, "weight:" f'{int(weight)}', (x + 400, y + 150))
-
+        cvzone.putTextRect(img, "weight:" f'{round(weight,2)}', (x + 400, y + 150))
+        start_time = actualtime
         #A,B,C =coff
         #difCM = A*dif**2 + B*dif + C
         #x1, y1, z1 = x2, y2, z2
